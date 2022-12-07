5462873f714aa118292ed5f36de1af5fadf28c53
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2022-12-07 12:07:15 +0800

    chore: add &no-check (#8)

--- supabase/functions/_utils/stripe.ts
+++ supabase/functions/_utils/stripe.ts
@@ -1,5 +1,5 @@
 // esm.sh is used to compile stripe-node to be compatible with ES modules.
-import Stripe from "https://esm.sh/stripe@10.13.0?target=deno&deno-std=0.132.0";
+import Stripe from "https://esm.sh/stripe@10.13.0?target=deno&deno-std=0.132.0&no-check";
 
 export const stripe = Stripe(Deno.env.get("STRIPE_SECRET_KEY") ?? "", {
   // This is needed to use the Fetch API rather than relying on the Node http
